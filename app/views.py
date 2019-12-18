e5e33ba805083a2487b2138937c483d5f9683bd3
xuezhan1999 <49605894+xuezhan1999@users.noreply.github.com>
2019-12-19 00:46:30 +0800

    Update views.py
    xz

--- app/views.py
+++ app/views.py
@@ -11,6 +11,7 @@ import json
 import redis
 from django_redis import get_redis_connection
 from django.core.cache import cache
+from django.core import serializers
 # Create your views here.
 
 from pymongo import MongoClient
@@ -44,10 +45,74 @@ def login(request):
     result = json.loads(postBody)
     user = User.objects.get(name=result['username'])
     if(user.pwd == result['password']):
-        request.session['username'] = result['username']
+        #request.session['username'] = result['username']
+        if user.expertid == None:
+            result = {"code": "0","isexpert": "-1","username":user.name}
+            return HttpResponse(json.dumps(result),content_type="application/json")
+        else:
+            result = {"code": "0", "isexpert": "0","username":user.name}
+            return HttpResponse(json.dumps(result), content_type="application/json")
+    else:
+        result = {"code": "-1", "isexpert": "-1"}
+        return HttpResponse(json.dumps(result), content_type="application/json")
+
+
+
+@api_view(['POST'])
+def message(request):
+    postBody = request.body
+    result = json.loads(postBody)
+    code = ''
+    for num in range(1,5):
+        code = code + str(random.randint(0,9))
+    tel = result['phone']
+
+    # Redis
+    cache.set(tel,code)
+
+    client = ZhenziSmsClient('https://sms_developer.zhenzikj.com','102458', 'f9ab7d85-bea0-48ef-875b-6895f4838061');
+    params = {'message': '您的验证码为:'+code, 'number': result['phone']};
+    send_result = client.send(params);
+    if(send_result != None):
         return HttpResponse(0)
     else:
         return HttpResponse(-1)
+
+@api_view(['POST'])
+def register(request):
+    postBody = request.body
+    result = json.loads(postBody)
+    user = User.objects.filter(name=result['username'])
+    if user:
+        return HttpResponse(-1)
+    else:
+        user = User.objects.create(name=result['username'], pwd=result['password'], balance=0.00,phone_num=result['phone'])
+        user.save()
+    return HttpResponse(0)
+
+@api_view(['POST'])
+def verify(request):
+    postBody = request.body
+    result = json.loads(postBody)
+    code = cache.get(result['phone'])
+    if(result['code'] == code):
+        return HttpResponse(0)
+    else:
+        return HttpResponse(-1)
+
+@api_view(['POST'])
+def islogin(request):
+    postBody = request.body
+    result = json.loads(postBody)
+    user = User.objects.filter(name=result['username'])
+    if user:
+        info = {"login" : "1"}
+        info2 = serializers.serialize("json",user)
+        info["userinfo"] = json.loads(info2)
+        return HttpResponse(json.dumps(info),content_type="application/json")
+    else:
+        info = {"login" : "-1","userinfo":"null"}
+        return HttpResponse(json.dumps(info),content_type="application/json")
 
 from django.shortcuts import render
 
